875a5fd3ab33e2cb4e0f6d31d725aba846fc8759
kiran pant <kiran.r.pant@gmail.com>
2024-05-14 12:37:43 +0545

    Adding a Creating Form

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const SnippetCreatePage = () => {
-  return <div>Create a Snippet!</div>;
+  return (
+    <form>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-8">
+          <label className="w-4" htmlFor="title">
+            Title
+          </label>
+          <input
+            className="border rounded p-2 w-full"
+            type="text"
+            name="title"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-8">
+          <label className="w-4" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            className="border rounded p-2 w-full"
+            name="code"
+            id="code"
+          />
+        </div>
+        <button type="submit" className="border rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default SnippetCreatePage;
