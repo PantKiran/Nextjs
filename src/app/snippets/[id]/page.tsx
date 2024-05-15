e0849bffdc7a0d00ba213ae4203b19f1a3bd0e11
kiran pant <kiran.r.pant@gmail.com>
2024-05-15 14:51:21 +0545

    feat:style the show page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -16,7 +16,20 @@ const SnippetShowPage = async (props: SnippetShowPagePropps) => {
   if (!Snippet) {
     return notFound();
   }
-  return <div>{Snippet.title}</div>;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center ">
+        <h1 className="text-xl font-bold">{Snippet.title}</h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{Snippet.code}</code>
+      </pre>
+    </div>
+  );
 };
 
 export default SnippetShowPage;
